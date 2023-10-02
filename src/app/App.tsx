b1ea558d4e87a334951cfa84c8614d8116911b8b
DmitriyZabava <102691652+DmitriyZabava@users.noreply.github.com>
2023-10-03 00:01:05 +0300

    first widget (navbar) and ui kit component (link)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,20 @@
 import "./styles/index.scss"
 
-import {NavLink} from "react-router-dom";
-
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
+import {NavBar} from "widgets/NavBar";
 
 
 function App() {
     const {theme, toggleTheme} = useTheme()
     return (
         <div className={classNames("app", {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-
-            <NavLink to={"/"}>ГЛАВНАЯ</NavLink>
-            <br/>
-            <NavLink to={"/about"}>О САЙТЕ</NavLink>
+            <NavBar/>
 
             <AppRouter/>
+
+            <button onClick={toggleTheme}>TOGGLE</button>
         </div>
     );
 }
