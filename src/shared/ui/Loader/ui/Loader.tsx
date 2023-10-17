cbe5116713b6d9052087c186f6f075e101cbc0e2
DmitriyZabava <102691652+DmitriyZabava@users.noreply.github.com>
2023-10-17 23:29:16 +0300

    loader refactor

--- src/shared/ui/Loader/ui/Loader.tsx
+++ src/shared/ui/Loader/ui/Loader.tsx
@@ -1,8 +1,16 @@
 import "./Loader.scss"
 
-export const Loader = () => {
+import { classNames } from "shared/lib/classNames/classNames";
+
+interface LoaderProps {
+    className?: string;
+}
+
+export const Loader = ({ className }:LoaderProps) => {
     return (
-        <div className={"lds-roller"}>
+        <div
+            className={classNames("lds-roller",{},[className])}
+        >
             <div></div>
             <div></div>
             <div></div>
