67eaeaed48ab958d8a028fdab2ea15e6836c42dd
DmitriyZabava <102691652+DmitriyZabava@users.noreply.github.com>
2023-11-14 22:26:23 +0300

    fix lint error

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -6,6 +6,7 @@ export function classNames(cls: string, mods: Mods = {}, additional: string[] = 
     return [cls,
         ...additional.filter(Boolean),
         ...Object.entries(mods)
+            // eslint-disable-next-line
             .filter(([_, value]) => Boolean(value))
             .map(([className]) => className)
     ].join(" ")
